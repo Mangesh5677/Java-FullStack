eec532b3595fd54b778e9ff1a6a641795fe33791
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-05 20:39:26 +0530

    Initial commit

---
+++ DoWhileDemo.java
@@ -0,0 +1,13 @@
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=3);
+        //in that first i=5 so execute the i = 5 then print i 
+        // so in that fiesr i =  5 assign so print the 5 
+        // next i ++ Means i become a 5+1 = 6
+        // then check the condition i <= 3 means 6 <= 3 is false so exit the loop
+    }
+}
