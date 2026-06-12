875091e77e50c6db068c158d81edc9992654b033
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-12 20:46:42 +0530

    Initial commit

--- EncapsulationDemo.java
+++ EncapsulationDemo.java
@@ -19,7 +19,7 @@ class Human{
 public class EncapsulationDemo {
     public static void main(String[] args) {
         Human obj = new Human();
-        obj.setAge(21);;
+        obj.setAge(21);
         obj.setName("Mangesh");
         System.out.println("Name: "+obj.getName());
         System.out.println("Age: "+obj.getAge());
